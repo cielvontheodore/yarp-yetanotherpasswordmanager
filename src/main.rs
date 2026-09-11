37a07ffa7ac5d3bf08e648f333dadf5db6b40028
cielvontheodore <mizakigaming89@gmail.com>
2026-09-11 14:45:56 +0700

    detik deitk sebelum menit lahir vault.yarp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    let command : Vec<String> = std::env::args().collect();    
+    let command : Vec<String> = std::env::args().collect();
+    if command[2] == "init" {
+        println!("Initializing YARP!");
+        std::fs::File::create("vault.yarp");
+    };
+
 }
