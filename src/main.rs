41ecf5fed4932e565e10d4b4e659a7461cb05230
cielvontheodore <mizakigaming89@gmail.com>
2026-09-11 17:52:01 +0700

    docs : just adding comments so i wouldn't be dumbfounded when i do this project again months later

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
     let command : Vec<String> = std::env::args().collect();
+    // apparently vec itu buat sesuatu yang nambah terus, std itu ngambil library rust abis itu
+    // ambil library env yang di bawaan rust lalu jalanin args tapi gua lupa args itu apa
     if command[2] == "init" {
         println!("Initializing YARP!");
-        // std::fs::File::create("vault.yarp");
+        // std::fs::File::create("vault.yarp"); this worked but yeah we're going harder than that,
+        // angh harder daddy
         std::fs::OpenOptions::new();
     };
 
