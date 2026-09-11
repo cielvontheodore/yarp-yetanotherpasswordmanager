0f731d5ec3327171bb1ec5a19a395c82434fa24f
cielvontheodore <mizakigaming89@gmail.com>
2026-09-11 14:54:16 +0700

    error handling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ fn main() {
     let command : Vec<String> = std::env::args().collect();
     if command[2] == "init" {
         println!("Initializing YARP!");
-        std::fs::File::create("vault.yarp");
+        // std::fs::File::create("vault.yarp");
+        std::fs::OpenOptions::new();
     };
 
 }
